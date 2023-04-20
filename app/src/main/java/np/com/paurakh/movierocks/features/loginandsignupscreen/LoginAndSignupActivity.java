ddf6d9501a9235480dc48899022524e67944895a
Paurakh1 <paurakhsaud2022@gmail.com>
2023-04-20 17:06:26 +0545

    added activity for login and signup and fragments for tab layout

--- app/src/main/java/np/com/paurakh/movierocks/features/loginandsignupscreen/LoginAndSignupActivity.java
+++ app/src/main/java/np/com/paurakh/movierocks/features/loginandsignupscreen/LoginAndSignupActivity.java
@@ -1,16 +1,24 @@
 package np.com.paurakh.movierocks.features.loginandsignupscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 import np.com.paurakh.movierocks.R;
 
 public class LoginAndSignupActivity extends AppCompatActivity {
+    TabLayout loginOrSignUpTab;
+    ViewPager loginOrSignUpViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_and_signup);
+
+        loginOrSignUpTab = findViewById(R.id.tlTabLayout_for_login_signup);
+        loginOrSignUpViewPager = findViewById(R.id.vpLoginViewPager);
     }
 }
